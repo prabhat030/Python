874ced201d77eb7a66c1bddae261b1af14b6c17e
prabhat030 <138551198+prabhat030@users.noreply.github.com>
2023-07-07 19:51:32 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,5 @@
+num1 = float(input("Enter num1: "))
+num2 = float(input("Enter num2: "))
+num3 = float(input("Enter num3: "))
+average = (num1+num2+num3)/3
+print("The average value of ",num1,",",num2," and ",num3," is:",average)
