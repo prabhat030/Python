a74b840f215bcf345fa7d4f2051c9a94b2592733
prabhat030 <138551198+prabhat030@users.noreply.github.com>
2023-07-09 14:46:20 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,88 @@
+# Arithmetic operator
+
+a= 60
+b= 80
+print("a+b:",a+b)
+print("a-b:",a-b)
+print("a*b:",a*b)
+print("a/b:",a/b)
+print("a**b:",a**b)
+print("a//b:",a//b)
+
+print()
+
+# Assignment operator
+
+c=60
+c+=3
+print("60+=3",c)
+c=60
+c-=3
+print("60-=3",c)
+c=60
+c*=3
+print("60*=3",c)
+c=60
+c/=3
+print("60/=3",c)
+c=60
+c%=3
+print("60%=3",c)
+c=60
+c//=3
+print("60//=3",c)
+c=60
+c**=3
+print("60**=3",c)
+
+print()
+
+# Comparison operator
+
+a = 60
+b= 80
+print("60>80 = ",a>b )
+print("60<80 = ",a<b )
+print("60>=80 = ",a>=b )
+print("60<=80 = ",a<=b )
+print("60==80 = ",a==b )
+print("60!=80 = ",a!=b )
+
+print()
+
+# Logical operator
+
+l =True
+m= False
+print(l,m)
+print("True and False =",l and m)
+print("True or False =",l or m)
+print("True l =",not l)
+
+print()
+
+# Bitwise operator
+
+print("60 | 80 =",a | b)
+print("60 & 80 =",a & b)
+print("60 ^ 80 =",a ^ b)
+print("60 >> 2 =",a >> 2)
+print("60 << 2 =",a << 2)
+
+print()
+
+# Identity operator
+
+x =10
+print("x:10")
+print("x is 10 =",x is 10)
+print("x is not 10 =",x is not 10)
+
+print()
+
+# Membership operator
+
+b = "Banglore"
+print(b)
+print("g in b:",("g" in b))
+print("g not in b:",("g" not in b))
